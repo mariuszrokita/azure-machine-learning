f50b78dce1beeace3ec9d4a9eaf47b23b5402f51
Mariusz Rokita <mariusz.rokita@gmail.com>
2019-06-16 00:29:04 +0200

    Run the app using either dev web server or production stack.

--- docker/flask-nginx-single-docker-image/launcher.sh
+++ docker/flask-nginx-single-docker-image/launcher.sh
@@ -1,3 +1,31 @@
 #!/bin/sh
-service nginx start
-uwsgi --ini uwsgi.ini
+if [ ! -f debug0 ]; then  
+  touch debug0
+
+  while getopts 'hd:' flag; do
+    case "${flag}" in
+      h)
+        echo "options:"
+        echo "-h        show brief help"
+        echo "-d        debug mode, no nginx or uwsgi, direct start with 'python3 app/app.py'"
+        exit 0
+        ;;
+      d)
+        touch debug1
+        ;;
+      *)
+        break
+        ;;
+    esac
+  done
+fi
+
+if [ -f debug1 ] 
+then  
+  echo "Running app in debug mode!"
+  python3 app/app.py
+else  
+  echo "Running app in production mode!"
+  service nginx start
+  uwsgi --ini uwsgi.ini
+fi
